3d66c94a57efdb3d997601319b64f2582656daa6
simon <wanwanzainaer@gmail.com>
2021-03-11 22:03:13 -0800

    Add navigation lab

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const CategoriesScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Categories Screen!!</Text>
+      <Button
+        title="GO to Meal"
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'MealDetail' });
+          // props.navigation.navigate('MealDetail');
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
+
+export default CategoriesScreen;
